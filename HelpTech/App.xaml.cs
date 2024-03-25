01345684e1ae6379e0fb8651daced6dbf2bfd3a3
WilliamSilvaGh <williamsilvaesquilino@gmail.com>
2024-03-25 20:16:41 -0300

    Adicionado novas Telas, Alinhamentos e Estilos

---
+++ HelpTech/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HelpTech
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ HelpTech/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace HelpTech
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
